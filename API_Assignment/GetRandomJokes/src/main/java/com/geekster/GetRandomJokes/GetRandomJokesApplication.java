e8632e69d4501c5ef61d846ba763e125730089f1
Supriya Ghorai <supriyagro2001@gmail.com>
2023-04-12 23:36:07 +0530

    added a new RandomJokes project

---
+++ API_Assignment/GetRandomJokes/src/main/java/com/geekster/GetRandomJokes/GetRandomJokesApplication.java
@@ -0,0 +1,39 @@
+package com.geekster.GetRandomJokes;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GetRandomJokesApplication {
+	public static void main(String[] args) throws Exception {
+		URL getUrl = new URL("https://api.chucknorris.io/jokes/random");
+		HttpURLConnection connection = (HttpURLConnection) getUrl.openConnection();
+		connection.setRequestMethod("GET");
+
+
+		int responseCode = connection.getResponseCode();
+
+		if (responseCode == 200) {
+
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			StringBuilder jsonResponseData = new StringBuilder();
+			String readLine = null;
+
+
+			while ((readLine = in.readLine()) != null) {
+				jsonResponseData.append(readLine);
+			}
+
+			in.close();
+			JSONObject masterData = new JSONObject(jsonResponseData.toString());
+			System.out.println(masterData.toString());
+
+		} else {
+			System.out.println("This is not valid URL- " + responseCode);
+		}
+	}
+}
