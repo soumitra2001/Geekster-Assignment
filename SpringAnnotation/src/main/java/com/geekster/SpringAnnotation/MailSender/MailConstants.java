e0a1a08ecefa8b49bd18016e6c6b058dac3b411a
Supriya Ghorai <78735395+soumitra2001@users.noreply.github.com>
2023-05-01 17:32:54 +0530

    Delete SpringAnnotation directory

--- SpringAnnotation/src/main/java/com/geekster/SpringAnnotation/MailSender/MailConstants.java
+++
@@ -1,9 +0,0 @@
-package com.geekster.SpringAnnotation.MailSender;
-
-public class MailConstants {
-
-    public static final String SENDER = "supriyagro2001@gmail.com";
-    public static final String RECEIVER = "soumitra01g@gamil.com";
-    public static final String SUBJECT = "This Mail is sending for Weekly Test: 16/04/2023";
-
-}
